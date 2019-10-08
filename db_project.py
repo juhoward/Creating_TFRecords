58f5eaadd3414a6c118b7397df397c3f34174f2f
Justin Howard <justinhoward@smu.edu>
2019-10-08 08:36:52 -0500

    added .py version due to github glitch with ipynb

---
+++ db_project.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import os
+os.chdir('c:/users/howar/documents/database_management/Term_project/')
+os.getcwd()
+
+# Path to ImageNet DET Data
+data_dir = "c:/users/howar/documents/database_management/Term_project/dataset/DET/train/ILSVRC2013_train/"
+
+# identify folders containing images in the training set
+folders = []
+def folder_list(data_dir):
+    global folders
+    folders = os.listdir(data_dir)
+    return
+folder_list(data_dir)
+# make list of images within the folders of the training set
+# identify folders containing images in the training set
+folders = []
+def folder_list(data_dir):
+    global folders
+    folders = os.listdir(data_dir)
+    return
+# make list of images within the folders of the training set
+images = []
+def gather_images(data_dir):
+    folder_list(data_dir)
+    global images
+    # uses the list of folders from folder_list
+    # makes images -> a list of images within 569 folders in data_dir
+    for folder in folders:
+        images.append(os.listdir(data_dir/folder))
+    return
+gather_images(data_dir)
