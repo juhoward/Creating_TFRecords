0d504d5aeb34f82c4534af580617fa241e08d5e2
Justin Howard <justinhoward@smu.edu>
2019-10-08 08:48:59 -0500

    fixed file path problem

--- db_project.py
+++ db_project.py
@@ -1,14 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-
-"""
-
 import os
+from pathlib import Path
 os.chdir('c:/users/howar/documents/database_management/Term_project/')
 os.getcwd()
 
 # Path to ImageNet DET Data
-data_dir = "c:/users/howar/documents/database_management/Term_project/dataset/DET/train/ILSVRC2013_train/"
+data_dir = Path("c:/users/howar/documents/database_management/Term_project/dataset/DET/train/ILSVRC2013_train/")
 
 # identify folders containing images in the training set
 folders = []
@@ -35,3 +31,6 @@ def gather_images(data_dir):
         images.append(os.listdir(data_dir/folder))
     return
 gather_images(data_dir)
+
+
+print(os.listdir(data_dir/folders[0]))
